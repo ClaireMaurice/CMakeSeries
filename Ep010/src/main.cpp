f0f33f2828f4e23f22daaa651c48e49b8ed59267
ClaireMaurice <maurice@emse.fr>
2026-02-18 16:44:22 +0100

    Organizing CMake code with add_subdirectory

---
+++ Ep010/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "stats.h"
+
+//#include "supermath.h"
+
+int main(int argc, char* argv[]) {
+    std::vector<int> values = {1, 2, 3, 4, 5};
+ 
+    double avg = mean(values.data(), values.size());
+    std::cout << "Mean: " << avg << std::endl;
+ 
+    return 0;
+}
